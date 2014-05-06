6220df5006d8cb22fc3948307394fac8ee6deba6
戚耿鑫 <qgx446738721@hotmail.com>
2014-05-06 21:16:53 +0800

    Update isap.cpp

--- 图论/isap.cpp
+++ 图论/isap.cpp
@@ -7,7 +7,7 @@ const int MAX_M = 999999;
 struct eage{
     int v, w, next;
 }road[MAX_M];
-int NE, NV;
+int NE, NV; //sap，init中的memset用到了NV，如果不能事先确定大小，最好改成初始化整个数组
 int head[MAX_N], lp;
 int gap[MAX_N], dis[MAX_N], pre[MAX_N], cur[MAX_N];
 void Init(){
@@ -27,7 +27,7 @@ void Add_Road(int s,int e,int len){
 int sap(int s,int t){
     memset(dis, 0, sizeof(int)*(NV + 1));
     memset(gap, 0, sizeof(int)*(NV + 1));
-    for(int i=1; i<=NV; i++) cur[i] = head[i];
+    for(int i=0; i<=NV; i++) cur[i] = head[i];
     int u = pre[s] = s, maxflow = 0, aug = -1;
     gap[0] = NV;
     while(dis[s] < NV){
