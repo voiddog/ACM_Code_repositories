5a797cc5b31c63625f742f26b5c310b451a2c540
qgx446738721 <qgx446738721@hotmail.com>
2014-07-28 15:57:46 +0800

    快速幂

--- 数论/快速幂取模.cpp
+++ 数论/快速幂取模.cpp
@@ -1,13 +1,9 @@
-ll mulmod(ll a,ll n,ll m){
-    // a*n mod m
-    ll y=0;
-    a%=m;
+ll powmod(ll a,ll n,ll m){
+    // a^n mod m
+    ll y=1;
     while(n){
-	if(n&1) y+=a;
-	if(y>=m) y-=m;
-	n>>=1;
-	a<<=1;
-	if(a>=m) a-=m;
+	if(n&1) y=y*a%m; //m 大于 int 范围则 y=mulmod(y,a,m);
+	if(n>>=1)a=a*a%m; //m 大于 int 范围则 a=mulmod(a,a,m);
     }
     return y;
 }
