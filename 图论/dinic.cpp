de9baa7990eb62d55df1d8562d72fb8e4f32510d
戚耿鑫 <qgx446738721@hotmail.com>
2014-08-22 16:20:40 +0800

    dinic
    比较快的dinic

---
+++ 图论/dinic.cpp
@@ -0,0 +1,58 @@
+#define sz 300005
+#define inf 0x7fffffff
+
+struct node
+{
+    int s,t,v,flag,nxt;
+} e[sz*20];
+int hd[sz],dis[sz],cur[sz],que[sz],cnt;
+void insert(int s,int t,int v)
+{
+    //cout << s << " " << t << " " << v << endl;
+    e[cnt].s=s,e[cnt].t=t,e[cnt].v=v,e[cnt].flag=0,e[cnt].nxt=hd[s],hd[s]=cnt++;
+    e[cnt].s=t,e[cnt].t=s,e[cnt].v=0,e[cnt].flag=0,e[cnt].nxt=hd[t],hd[t]=cnt++;
+}
+bool bfs(int s,int t,int n)
+{
+    int head=0,tail=0,i;
+    memset(dis,-1,sizeof(dis[0])*(n+1)),dis[s]=0;
+    que[tail++]=s;
+    while(head<tail)
+    {
+        for(i=hd[que[head++]]; i!=-1; i=e[i].nxt)
+            if(e[i].v>0&&dis[e[i].t]==-1)
+            {
+                dis[e[i].t]=dis[e[i].s]+1;
+                if(e[i].t==t)return 1;
+                que[tail++]=e[i].t;
+            }
+    }
+    return 0;
+}
+int dinic(int s,int t,int n){
+    int i,mf=0,tp;
+    while(bfs(s,t,n)){
+        for(i=0; i<n; i++)cur[i]=hd[i];
+        int u=s,tail=0;
+        while(cur[s]!=-1){
+            if(u!=t&&cur[u]!=-1&&e[cur[u]].v>0&&dis[u]+1==dis[e[cur[u]].t]){
+                que[tail++]=cur[u];
+                u=e[cur[u]].t;
+            }
+            else if(u==t){
+                for(tp=inf,i=tail-1; i>=0; i--)tp=min(tp,e[que[i]].v);
+                for(mf+=tp,i=tail-1; i>=0; i--{
+                    e[que[i]].v-=tp;
+                    e[que[i]^1].v+=tp;
+                    if(e[que[i]].v==0)tail=i;
+                }
+                u=e[que[tail]].s;
+            }
+            else{
+                while(u!=s&&cur[u]==-1)u=e[que[--tail]].s;
+                cur[u]=e[cur[u]].nxt;
+            }
+        }
+    }
+    return mf;
+}
